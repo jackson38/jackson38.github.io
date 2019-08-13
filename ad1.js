197a565913393e417d8a67d4041af12b806fbecd
jackson38 <50540048+jackson38@users.noreply.github.com>
2019-08-14 02:06:45 +0800

    Update ad1.js

--- ad1.js
+++ ad1.js
@@ -1,1 +1,1 @@
-document.write ('<script type="text/javascript"  src="https:////js.users.51.la/20037735.js"></script>');
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/20037735.js"></script>');
